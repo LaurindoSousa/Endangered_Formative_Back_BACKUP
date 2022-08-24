fc3850dbb96538aa1f48f359866418c939db5f5d
Laurindo Sousa <laurindosousa@ymail.com>
2022-08-24 15:16:25 +1200

    changes

--- server.js
+++ server.js
@@ -11,22 +11,7 @@ app.get("/", (req, res) => {
 });
 
 
-app.delete("/animals/:id", (req,res) => {
-    console.log(req.params.id);
-    
-    const rawData = fs.readFileSync("./data.json");
-    const data = JSON.parse(rawData);
-    const machIndex = data.animals.findIndex((animals) => {
-        return animals.id === req.params.id;
-    });
-    
-    data.animals.splice(matcIndex, 1);
-    
-    const newJson = JSON.stringify(data);
-    fs.writeFileSync("./data.json", newJson);
-    
-    res.send("deleted: " + req.params.id);
-});
+
 
 app.listen(3000, () => {
     console.log("Listening to port 3000")
